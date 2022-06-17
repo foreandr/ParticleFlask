d7077a6d61691f343b55e71472b923a686be9731
Andre <foreandr@gmail.com>
2022-06-17 15:32:40 -0400

    site

---
+++ static/js/lib/blog_onload.js
@@ -0,0 +1,10 @@
+function load_theology(){
+    var praise = `<br>Oh, great star! What would your happiness be if not for those on whom you shine?<br>
+                In my deepest wound I saw your glory, and it dazzled me.<br>
+                Every creature who is moved to attain and perceive Him will be cast back by the splendour of His majesty, nor is there anyone who cranes his neck to see Him whose glance is not turned aside in amazement.<br>
+                The highest function of the soul of man is the perception of truth; in this accordingly it finds its special delight.<br>
+                When love to God is complete, no joy is equal to the joy of worship.<br>
+                Thou hast made us for thyself, O Lord, and our heart is restless until it finds its rest in thee.<br>`
+
+    document.getElementById("stanza").innerHTML += praise
+}
